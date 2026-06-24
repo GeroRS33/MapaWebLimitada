f240ffc9feb0185d85efc46315fde0b72b006789
GeroRS33 <gerorevetria2@gmail.com>
2026-06-24 11:48:37 -0300

    Actualización general del proyecto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import logoWeb from '../assets/icons/LogoWeb.svg';
+import logoWeb from '../assets/icons/Logo_MapaWeb.svg';
 import iconoUbicacion from '../assets/icons/Icono-Ubicacion.svg';
 
 export default function Header() {
   return (
     <header className="app-header" id="app-header">
       <div className="header-logo-container">
-        <img 
-          src={logoWeb} 
-          alt="WEB logo" 
-          className="header-logo" 
+        <img
+          src={logoWeb}
+          alt="WEB logo"
+          className="header-logo"
           id="header-logo-img"
         />
       </div>
       <div className="header-title-container" id="header-title-wrapper">
-        <img 
-          src={iconoUbicacion} 
-          alt="Ubicación" 
-          className="header-title-icon" 
+        <img
+          src={iconoUbicacion}
+          alt="Ubicación"
+          className="header-title-icon"
           id="header-title-icon-img"
         />
         <h1 className="header-title" id="header-main-title">Puntos de Venta</h1>
